b89b7ba720ac6b011c6e7452af420eba618ff876
EwanSmith811 <107436087+EwanSmith811@users.noreply.github.com>
2024-01-06 13:06:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://realtime-database-2fb12-default-rtdb.firebaseio.com"
+    databaseURL: "https://shopping-cart-7ed98-default-rtdb.firebaseio.com/"
 }
 
 const app = initializeApp(appSettings)
